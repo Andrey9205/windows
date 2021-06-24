e7deb596255db072347cb9dc5692c9b52e003057
Andrey <aisuk9607@gmail.com>
2021-06-24 22:37:32 +0300

    windows

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -19,7 +19,6 @@ const modals = () => {
 				modal.style.display = "block";
 				document.body.style.overflow = "hidden";
 				document.body.style.marginRight = `${scroll}px`;
-				// document.body.classList.add('modal-open');
 			});
 		});
 
@@ -33,7 +32,6 @@ const modals = () => {
 			document.body.style.marginRight = `0x`;
 
 
-			// document.body.classList.remove('modal-open');
 		});
 
 		modal.addEventListener('click', (e) => {
@@ -46,7 +44,6 @@ const modals = () => {
 				document.body.style.overflow = "";
 				document.body.style.marginRight = `0px`;
 
-				// document.body.classList.remove('modal-open');
 			}
 		});
 	}
@@ -79,7 +76,7 @@ const modals = () => {
 	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
 	bindModal('.popup_calc_button', '.popup_calc_profile', '.popup_calc_profile_close', false);
 	bindModal('.popup_calc_profile_button', '.popup_calc_end', '.popup_calc_end_close', false);
-	//showModalByTime('.popup', 60000);
+	showModalByTime('.popup', 60000);
 };
 
 export default modals;
